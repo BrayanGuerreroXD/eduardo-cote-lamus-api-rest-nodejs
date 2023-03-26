d35f2351632ec3f38816110f1e733efbfcd7b14b
BrayanGuerreroXD <bg7047653@gmail.com>
2023-03-26 13:53:08 -0500

    Add new scripts

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import studentsRoutes from "./routes/students.routes.js";
+import registersRoutes from "./routes/registers.routes.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/", studentsRoutes);
+app.use("(api/", registersRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({
+    message: "Not found",
+  });
+});
+
+export default app
